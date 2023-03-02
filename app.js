4fa8b478b71acf3454cba0732cfa9375a2b3905f
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-03-02 23:02:11 +0800

    fix(login): set storage error

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 App({
-
-    onShow() {
+    onLaunch() {
         const token = wx.getStorageSync('token')
 
-        if(!token) {
+        if (!token) {
             wx.login({
-              timeout: 10 * 1000,
-              success({ code }) {
-                  wx.request({
-                    url: 'https://cg-api.imzbb.cc/login',
-                    data: { code },
-                    method: 'POST',
-                    success(res) {
-                        if(res.statusCode === '200') {
-                            wx.setStorage('token', res.data.token)
-                        } else {
-                            wx.showToast({
-                              title: '登录失败',
-                              icon: 'error'
-                            })
+                timeout: 10 * 1000,
+                success({ code }) {
+                    wx.request({
+                        url: 'https://cg-api.imzbb.cc/login',
+                        data: { code },
+                        method: 'POST',
+                        success(res) {
+                            if (res.statusCode == 200) {
+                                wx.setStorage({ key: 'token', data: res.data.token })
+                            } else {
+                                wx.showToast({
+                                    title: '登录失败',
+                                    icon: 'error'
+                                })
+                            }
                         }
-                    }
-                  })
-              }
+                    })
+                }
             })
         }
     },
