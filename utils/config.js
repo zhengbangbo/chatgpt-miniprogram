742c9bf1319f749ee8abbd7f5571cc6a970a1614
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-03-05 16:35:20 +0800

    fix(css): maxlength="-1"

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,6 @@
-export const BACKEND_URL_BASE = 
-    wx.getAccountInfoSync().miniProgram.envVersion === "release" ?
-    // 正式版
-        "https://cg-api.imzbb.cc" :
-    // 体验版 + 开发版
-        "http://192.168.31.236:4897"
-export const APP_VERSION = "0.2.0"
+export const BACKEND_URL_BASE = wx.getAccountInfoSync().miniProgram.envVersion === "develop"
+    ? "http://192.168.31.236:4897"  // 开发版
+    : wx.getAccountInfoSync().miniProgram.envVersion === "trial"
+        ? "https://cg-api.imzbb.cc"  // 体验版
+        : "https://cg-api.imzbb.cc"; //正式版
+export const APP_VERSION = "0.2.1"
