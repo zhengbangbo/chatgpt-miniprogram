a82fa6ec43fb9e9b3b74fe0b96246e679194ec2d
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-03-23 00:05:54 +0800

    chore: release v0.4.2
    * large font mode

--- utils/config.js
+++ utils/config.js
@@ -4,4 +4,4 @@ export const BACKEND_URL_BASE = wx.getAccountInfoSync().miniProgram.envVersion =
     : wx.getAccountInfoSync().miniProgram.envVersion === "trial"
         ? "https://cg-api2.imzbb.cc"  // 体验版
         : "https://cg-api2.imzbb.cc"; //正式版
-export const APP_VERSION = "0.4.1"
+export const APP_VERSION = "0.4.2"
