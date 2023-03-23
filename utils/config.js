9592e1a6495ace57212677ee3a726a5997ec66d9
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-03-23 13:11:53 +0800

    feat: add share note

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 export const BACKEND_URL_BASE = wx.getAccountInfoSync().miniProgram.envVersion === "develop"
-    // ? "http://localhost:4897"  // 开发版
-    ? "https://cg-api2.imzbb.cc"  // 开发版
+    ? "http://localhost:4897"  // 开发版
+    // ? "https://cg-api2.imzbb.cc"  // 开发版
     : wx.getAccountInfoSync().miniProgram.envVersion === "trial"
         ? "https://cg-api2.imzbb.cc"  // 体验版
         : "https://cg-api2.imzbb.cc"; //正式版
-export const APP_VERSION = "0.4.2"
+export const APP_VERSION = "0.4.3"
